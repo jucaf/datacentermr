80b0a5a30034a12c8133e61945aea7a670083741
Juan Carlos Fernandez <jcfernandez@redoop.org>
2014-08-29 14:55:42 +0200

    Replace date parsing by string replace

--- EsFeederMapper.java
+++ EsFeederMapper.java
@@ -3,8 +3,6 @@ package datacentermr;
 import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.MapWritable;
@@ -22,25 +20,14 @@ public class EsFeederMapper extends Mapper<Object, Text, NullWritable, MapWritab
 	 
 		MapWritable doc = new MapWritable();
 		String[] line = value.toString().split(",");
-		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		
 		
 		doc.put(new Text("hcan_SiteName"), new Text(line[1]));
 		doc.put(new Text("hcan_RoomName"), new Text(line[2]));
-		Date date = null;
-		try {
-			date = dateFormat.parse(line[3]);
-			long time = date.getTime();
-			doc.put(new Text("hcan_Fecha"), new FloatWritable(time));
-			doc.put(new Text("hcan_Power"), new FloatWritable(Float.parseFloat(line[4])));
-			doc.put(new Text("hcan_Temp"), new FloatWritable(Float.parseFloat(line[5])));
-			doc.put(new Text("hcan_Humidity"), new FloatWritable(Float.parseFloat(line[6])));
-			date = dateFormat.parse(line[7]);
-			time = date.getTime();
-			doc.put(new Text("hcan_Timestamp"), new FloatWritable(time));
-			context.write(NullWritable.get(), doc);
-		} catch (ParseException e) {
-			log.error(e.getMessage());
-		}
+		doc.put(new Text("hcan_Fecha"), new Text(line[3].replace(' ','T')));
+		doc.put(new Text("hcan_Power"), new FloatWritable(Float.parseFloat(line[4])));
+		doc.put(new Text("hcan_Temp"), new FloatWritable(Float.parseFloat(line[5])));
+		doc.put(new Text("hcan_Humidity"), new FloatWritable(Float.parseFloat(line[6])));
+		doc.put(new Text("hcan_Timestamp"), new Text(line[6].replace(' ','T')));
+		context.write(NullWritable.get(), doc);
  }
 }
